0456795bd7a8e783c10ccc5e96adb8c9b34973f7
jbw <jbw@users.noreply.github.com>
2018-01-24 22:10:29 +0000

    add empty activity types tests

--- test/activity-types.spec.js
+++ test/activity-types.spec.js
@@ -1,3 +1,35 @@
-describe('#Activity Types', () => {
+const chai = require('chai');
+const expect = chai.expect;
+const nock = require('nock');
+const test = require('./common.js');
+const api = test.api;
+const fitbitDomain = test.fitbitDomain;
 
+describe('#Activity Types', () => {
+    beforeEach(() => {
+    });
+
+    after(() => {
+        nock.cleanAll();
+    });
+
+    it('should get activity types list', (done) => {
+
+    });
+
+    it('should get activity type detail', (done) => {
+
+    });
+
+    it('should get frequent activity types', (done) => {
+
+    });
+
+    it('should get recent activity types', (done) => {
+
+    });
+
+    it('should get favourite activity types', (done) => {
+
+    });
 });
