16df853a3d6946884f1da05f6b328664c3f98991
jbw <jbw@users.noreply.github.com>
2018-01-24 22:10:05 +0000

    add empty activity logs tests

--- test/activity-logging.spec.js
+++ test/activity-logging.spec.js
@@ -1,4 +1,24 @@
-describe('#Activity Logging', () => {
+const chai = require('chai');
+const expect = chai.expect;
+const nock = require('nock');
+const test = require('./common.js');
+const api = test.api;
+const fitbitDomain = test.fitbitDomain;
 
+describe('#Activity Logging', () => {
+    beforeEach(() => {
+    });
+
+    after(() => {
+        nock.cleanAll();
+    });
+
+    it('should get activity logs list', (done) => {
+
+    });
+
+    it('should get activity TCX', (done) => {
+
+    });
 });
 
