e6733967d8b3e82815d820fb1809a53d227112e2
jbw <jbw@users.noreply.github.com>
2018-01-24 22:09:54 +0000

    add empty activity goals tests

--- test/activity-goals.spec.js
+++ test/activity-goals.spec.js
@@ -1,3 +1,19 @@
-describe('#Activity Goals', () => {
+const chai = require('chai');
+const expect = chai.expect;
+const nock = require('nock');
+const test = require('./common.js');
+const api = test.api;
+const fitbitDomain = test.fitbitDomain;
 
+describe('#Activity Goals', () => {
+    beforeEach(() => {
+    });
+
+    after(() => {
+        nock.cleanAll();
+    });
+
+    it('should get activity goals', (done) => {
+
+    });
 });
