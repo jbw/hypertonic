b91eebc056cba3a3d8fed3902a64555f6a8f0154
Jason Watson <jbw@users.noreply.github.com>
2018-01-31 21:23:59 +0000

    add tests for food/water  goals/logs

--- test/food-logging.spec.js
+++ test/food-logging.spec.js
@@ -7,55 +7,130 @@ const fitbitDomain = test.fitbitDomain;
 
 describe('#Food Logging', () => {
 
+    beforeEach(() => {
+        const locales = require('./fixtures/food-locales.json');
+        const goals = require('./fixtures/food-goals.json');
+        const logs = require('./fixtures/food-logs.json');
+        const waterLog = require('./fixtures/food-water-logs.json');
+        const waterGoal = require('./fixtures/food-water-goal.json');
+
+        nock(fitbitDomain)
+            .get('/1/foods/locales.json')
+            .reply(200, locales);
+        nock(fitbitDomain)
+            .get('/1/user/-/foods/log/goal.json')
+            .reply(200, goals);
+        nock(fitbitDomain)
+            .get('/1/user/-/foods/log/date/2017-01-01.json')
+            .reply(200, logs);
+        nock(fitbitDomain)
+            .get('/1/user/-/foods/log/water/date/2017-01-01.json')
+            .reply(200, waterLog);
+        nock(fitbitDomain)
+            .get('/1/user/-/foods/log/water/goal.json')
+            .reply(200, waterGoal);
+
+    });
+
+    after(() => {
+        nock.cleanAll();
+    });
+
+    it('should get food locales', (done) => {
+        api.getFoodLocales().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
+    });
+
     it('should get food goals', (done) => {
-        done(new Error());
+        api.getFoodGoals().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get food logs', (done) => {
-        done(new Error());
+        api.getFoodLogs().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get water logs', (done) => {
-        done(new Error());
+        api.getWaterLogs().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get water goals', (done) => {
-        done(new Error());
+        api.getWaterGoals().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get food time series', (done) => {
-        done(new Error());
+        api.getFoodTimeSeries().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get water time series', (done) => {
-        done(new Error());
+        api.getWaterTimeSeries().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
-    it('should get favourite foods', (done) => {
-        done(new Error());
+    it('should get favorite foods', (done) => {
+        api.getFavoriteFoods().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get frequent foods', (done) => {
-        done(new Error());
+        api.getFrequentFood().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get recent foods', (done) => {
-        done(new Error());
+        api.getRecentFood().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get meals', (done) => {
-        done(new Error());
+        api.getMeals().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get a single meal', (done) => {
-        done(new Error());
+        api.getMeal('meal_id').then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get food', (done) => {
-        done(new Error());
+        api.getFood().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 
     it('should get food units', (done) => {
-        done(new Error());
+        api.getFoodUnits().then(json => {
+            expect(json).to.not.be.undefined;
+            done();
+        }).catch(err => done(new Error(JSON.stringify(err))));
     });
 });
