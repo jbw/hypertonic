35f0406380636e3a5790c3ce361c676e54b4aa2c
jbw <jbw@users.noreply.github.com>
2018-02-02 23:13:58 +0000

    update readme and example

--- example/index.js
+++ example/index.js
@@ -3,68 +3,68 @@ const oauth2 = require('simple-oauth2');
 const express = require('express');
 const Hypertonic = require('../src/api');
 
-const secrets_path = '../secrets/';
-const credentials = require(secrets_path + 'fitbit.json');
-let token = require(secrets_path + 'token.json').token;
+const fitbitConfig = require('./fitbit.json');
 
 const serverConfig = {
     callbackHost: 'http://localhost:3000',
     callbackPath: '/auth/fitbit/callback',
     refreshPath: '/refresh',
-    authPath: '/auth'
+    authPath: '/'
 };
 
-const auth = oauth2.create(credentials.fitbit);
-const authUrl = auth.authorizationCode.authorizeURL({ scope: credentials.config.scopes });
+const auth = oauth2.create(fitbitConfig.credentials);
+const authUrl = auth.authorizationCode.authorizeURL({ scope: fitbitConfig.settings.scopes });
 
-let _hypertonic = null;
+let hypertonic = null;
 
 const app = express();
 
-app.get(serverConfig.refreshPath, (req, res) => {
-    const tokenObj = auth.accessToken.create(token);
+let _token = null;
 
-    tokenObj.refresh().then(newAccessToken => {
-        token = newAccessToken;
-        return res.status(200).json(token);
+const _saveToken = (token) => {
+    const savePath = './token.json';
+
+    fs.writeFile(savePath, JSON.stringify(token, null, 2), function (err) {
+        if (err) console.log(err);
+        console.log('Token saved token to ' + savePath);
     });
-});
-
-app.get(serverConfig.authPath, (req, res) => res.redirect(authUrl));
+};
 
 app.get(serverConfig.callbackPath, (req, res) => {
 
     const tokenConfig = {
         code: req.query.code,
-        expires_in: credentials.config.expires_in
+        expires_in: fitbitConfig.settings.expires_in
     };
 
     auth.authorizationCode.getToken(tokenConfig, (err, result) => {
 
         if (err) return res.json(err);
 
-        token = auth.accessToken.create(result);
-        const savePath = '../secrets/token.json';
+        _token = auth.accessToken.create(result);
+        _saveToken(_token);
 
-        fs.writeFile(savePath, JSON.stringify(token, null, 2), function (err) {
-            if (err) console.log(err);
-            console.log('Token saved token to ' + savePath);
-        });
-
-        return res.status(200).json(token);
+        return res.status(200).json(_token);
 
     });
-
 });
 
 app.get('/example', (req, res) => {
 
-    _hypertonic = Hypertonic(token.access_token);
-    _hypertonic.getActivities().fetch().then(json => {
+    Hypertonic(_token).getActivities().fetch().then(json => {
         return res.status(200).json(json);
     });
-
 });
+
+app.get(serverConfig.refreshPath, (req, res) => {
+    _token.refresh().then(newAccessToken => {
+        _token = newAccessToken;
+        return res.status(200).json(_token);
+    });
+});
+
+app.get(serverConfig.authPath, (req, res) => res.redirect(authUrl));
+
 
 console.log('Server started at:', serverConfig.callbackHost);
 app.listen(3000);
