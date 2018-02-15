bdf9b429a6fe67d5bd0b3bc9eded039be8962cff
jbw <jbw@users.noreply.github.com>
2018-02-15 00:24:23 +0000

    test definitions

--- typings/tests/main.spec.ts
+++ typings/tests/main.spec.ts
@@ -1,28 +1,16 @@
-import * as chai from 'chai';
-import 'mocha';
-const expect = chai.expect;
-import * as nock from 'nock';
 
-import Hypertonic from '../hypertonic';
+
+import Hypertonic from '../..';
 import { Friends } from '../friends';
 import { SleepLogs } from '../sleeps';
 
-
 const hypertonic = Hypertonic('token');
 
-describe('#Typescript', () => {
+const friends = hypertonic.getFriends().then((data: Friends) => {
+    console.log(data.friends[0].user.city);
+});
 
-    it.only('Main', (done: any) => {
-
-        const friends = hypertonic.getFriends().then((data: Friends) => {
-            console.log(data.friends[0].user.city);
-            done()
-        });
-
-        const sleepLogs = hypertonic.getSleepLogs('').then((data: SleepLogs) => {
-            console.log(data.sleep[0].dateOfSleep);
-            done()
-        });
-    });
+const sleepLogs = hypertonic.getSleepLogs('').then((data: SleepLogs) => {
+    console.log(data.sleep[0].dateOfSleep);
 });
 
