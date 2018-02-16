6f32512cfda1f82245c6cffad9b3058d914e1ead
jbw <jbw@users.noreply.github.com>
2018-02-16 20:56:31 +0000

    remove config and set target bundle size

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -13,26 +13,11 @@ module.exports = function (env) {
 
         plugins: [
 
-            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
             new webpack.DefinePlugin({
                 'process.env': {
                     'NODE_ENV': JSON.stringify('production')
                 }
             }),
-
-            // new webpack.optimize.CommonsChunkPlugin({
-            //     name: 'vendor',
-            //     children: true,
-            //     minChunks(module) {
-            //         return module.context && -1 !== module.context.indexOf('node_modules');
-            //     }
-            // }),
-
-            // new webpack.optimize.CommonsChunkPlugin({
-            //     name: 'manifest',
-            //     minChunks: Infinity
-            // }),
 
             new webpack.optimize.UglifyJsPlugin({
                 beautify: false,
@@ -51,26 +36,7 @@ module.exports = function (env) {
                 output: {
                     comments: false // allow comments in debug mode
                 }
-            }),
-
-
-            new webpack.optimize.AggressiveMergingPlugin(),
-
-            new webpack.optimize.MinChunkSizePlugin({
-                minChunkSize: 50000
-            }),
-
-            new webpack.HashedModuleIdsPlugin(),
-
-            new webpack.optimize.OccurrenceOrderPlugin(true)
-
-            /*new CompressionPlugin({
-                asset: '[path].gz[query]',
-                algorithm: 'gzip',
-                test: /\.js$|\.css$|\.html$/,
-                threshold: 10240,
-                minRatio: 0.8
-            })*/
+            })
         ]
     });
 };
