91a3eb9ef2ad79b8535de94465cc3384511b6037
jbw <jbw@users.noreply.github.com>
2018-02-19 22:11:28 +0000

    add user type

---
+++ typings/user.d.ts
@@ -0,0 +1,32 @@
+
+export interface User {
+    aboutMe: string;
+    avatar: string;
+    avatar150: string;
+    avatar640: string;
+    city: string;
+    clockTimeDisplayFormat: string;
+    country: string;
+    dateOfBirth: string;
+    displayName: string;
+    distanceUnit: string;
+    encodedId: string;
+    foodsLocale: string;
+    fullName: string;
+    gender: string;
+    glucoseUnit: string;
+    height: string;
+    heightUnit: string;
+    locale: string;
+    memberSince: string;
+    offsetFromUTCMillis: string;
+    startDayOfWeek: string;
+    state: string;
+    strideLengthRunning: string;
+    strideLengthWalking: string;
+    timezone: string;
+    waterUnit: string;
+    weight: string;
+    weightUnit: string;
+}
+
